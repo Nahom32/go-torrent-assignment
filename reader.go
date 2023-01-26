45b1331d2022f1190f096cb3c83d9d8ffc8b2efe
Nahom32 <senaynahom00@gmail.com>
2023-01-26 19:42:26 +0300

    added a marshalling algorithm

--- reader.go
+++ reader.go
@@ -2,8 +2,11 @@ package reader
 
 import (
 	"crypto/sha1"
+	"log"
+	"io"
 	"rand"
-	"github.com/jackpal/bencode-go"
+
+	bencode "github.com/jackpal/bencode-go"
 )
 
 type  torrentfile struct{
@@ -13,5 +16,20 @@ type  torrentfile struct{
 	length int
 	name string
 }
+type bencodeTorrentFile struct{
+	announcer string 
+	torrent torrentfile 
+}
+func changetoReadable(r *io.Reader)(interface{}){
+	t := bencodeTorrentFile{}
+	err := bencode.Unmarshal(*r,&t)
+	if err != nil {
+		log.Printf("Error caused as a result of unmarshalling")
+	}
+	return &r
+}
+
+
+
 
 
