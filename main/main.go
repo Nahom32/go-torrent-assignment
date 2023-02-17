c4b8c208ffa914194268d48a4171b7778ce31320
Nahom32 <senaynahom00@gmail.com>
2023-02-17 09:59:28 +0300

    added a temporary hasher

---
+++ main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	//"gotorrentassignment"
+)
+
+func main(){
+
+}
--- reader.go
+++ reader.go
@@ -2,12 +2,21 @@ package reader
 
 import (
 	"crypto/sha1"
-	"log"
+	
 	"io"
-	"rand"
+	"log"
+	"os"
+
+	//"rand"
 
 	bencode "github.com/jackpal/bencode-go"
 )
+type bencoded struct{
+	pieces string
+	plength int
+	wlength int
+	name string
+}
 
 type  torrentfile struct{
 	announcer string
@@ -18,15 +27,40 @@ type  torrentfile struct{
 }
 type bencodeTorrentFile struct{
 	announcer string 
-	torrent torrentfile 
+	torrent bencoded
 }
-func changetoReadable(r *io.Reader)(interface{}){
+
+
+func changetoReadable(r *io.Reader)(*bencodeTorrentFile,error){
 	t := bencodeTorrentFile{}
 	err := bencode.Unmarshal(*r,&t)
 	if err != nil {
 		log.Printf("Error caused as a result of unmarshalling")
 	}
-	return &r
+	return &t,nil;
+}
+
+// func parseFile(t bencodeTorrentFile)(interface{}){
+// 	var reader io.Reader
+// 	val := ()
+
+// }
+func hasher(val string)([][20]byte,error){
+	var fileHolder [][20]byte
+	x,err:= os.ReadFile(val)
+	if err != nil{
+		return nil,err
+	}
+	for i,_ := range x{
+		if i + 20 < len(fileHolder){
+			fileHolder= append(fileHolder,sha1.Sum(x[i:i+20]))
+		}else {
+			fileHolder= append(fileHolder,sha1.Sum(x[i:i+20]))
+		}
+		fileHolder= append(fileHolder,sha1.Sum(x[i:len(val)-1]))
+	}
+	
+	return fileHolder,nil
 }
 
 
